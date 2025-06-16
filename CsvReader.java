819c38b2d3d44a5e44d2df63680fa7f0c5f8faf1
Giordano D. Serafini <gdserafini@outlook.com>
2025-06-16 15:23:46 -0300

    Update CsvReader.java

--- CsvReader.java
+++ CsvReader.java
@@ -1,3 +1,43 @@
-public class CsvReader {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class CsvReader {
+    private String url;
+    private ArrayList<String> lines;
+
+    public FileDataReader(String url){
+        this.url = url;
+        this.lines = new ArrayList<String>();
+    }
+
+    public FileDataReader read(){        
+        try (var br = new BufferedReader(new FileReader(this.url))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                this.lines.add(line.trim());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return this;
+    }    
+
+    public ArrayList<Integer> getAsIntegers() {
+        ArrayList<Integer> integers = new ArrayList<>();
+      
+        for (String line : this.lines) {
+            try {
+                integers.add(Integer.parseInt(line));
+            } catch (NumberFormatException e) {
+                System.err.println("Valor inválido ignorado: " + line);
+            }
+        }
+      
+        return integers;
+    }
+  
+    public ArrayList<String> get(){ return this.lines; }
 }
