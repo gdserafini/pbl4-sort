fa372ff7a54f1b806e1cfe7a3a9488769ebee54b
Giordano D. Serafini <gdserafini@outlook.com>
2025-06-16 15:16:20 -0300

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,9 +1,22 @@
 public class InsertionSort implements Sort{
 
     @Override
-    public Comparable[] sort(Comparable[] values) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sort'");
+    public T[] sort(T[] values) {
+        if (values == null || values.length == 0) return values;
+
+        for (int i = 1; i < values.length; i++) {
+            T current = values[i];
+            int j = i - 1;
+
+            while (j >= 0 && values[j].compareTo(current) > 0) {
+                values[j + 1] = values[j];
+                j--;
+            }
+
+            values[j + 1] = current;
+        }
+
+        return values;
     }
 
 }
