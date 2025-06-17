32a5627b03c43dbaae453b65fee7067e5314e939
gdserafini <gdserafini@outlook.com>
2025-06-17 18:49:24 -0300

    update

--- Main.java
+++ Main.java
@@ -3,25 +3,39 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        String[] types = {"aleatorio", "crescente", "decrescente"};
+
+        String[] types = {
+            "aleatorio", 
+            "crescente", 
+            "decrescente"
+        };
         int[] sizes = {100, 1000, 10000};
+
+        @SuppressWarnings("unchecked")
         Sort<Integer>[] algorithms = new Sort[]{
             new BubbleSort<>(),
             new InsertionSort<>(),
             new QuickSort<>()
         };
-        String[] algorithmsNames = {"Bubble Sort", "Insertion Sort", "Quick Sort"};
+
+        String[] algorithmsNames = {
+            "Bubble Sort", 
+            "Insertion Sort", 
+            "Quick Sort"
+        };
 
         System.out.printf("%-15s %-10s %-15s %-15s\n", "Tipo", "Tamanho", "Algoritmo", "Tempo (ms)");
         System.out.println("---------------------------------------------------------------");
 
         for (String type : types) {
             for (int size : sizes) {
+                
                 String path = String.format("./data/%s_%d.csv", type, size);
                 CsvReader reader = new CsvReader(path).read();
                 ArrayList<Integer> originalData = reader.getAsIntegers();
 
                 for (int i = 0; i < algorithms.length; i++) {
+                    
                     Sort<Integer> algorithm = algorithms[i];
                     String name = algorithmsNames[i];
 
@@ -35,8 +49,9 @@ public class Main {
 
                     System.out.printf("%-15s %-10d %-15s %-15d\n", type, size, name, time);
                 }
+
+                System.out.println();
             }
         }
     }
 }
-
